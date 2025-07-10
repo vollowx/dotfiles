46b4d4178921418ae930f45aecb96afb0f27912a
vollowx <vollowx@outlook.com>
2025-07-10 23:46:10 +0800

    feat(neovim): enable virtual lines for diagnostic

--- cli/.config/nvim/lua/core/_internal/lsp.lua
+++ cli/.config/nvim/lua/core/_internal/lsp.lua
@@ -196,6 +196,14 @@ local function setup_diagnostic()
     virtual_text = {
       spacing = 4,
       prefix = vim.trim(icons.ui.AngleLeft),
+      severity = {
+        max = vim.diagnostic.severity.WARN,
+      },
+    },
+    virtual_lines = {
+      severity = {
+        min = vim.diagnostic.severity.ERROR,
+      },
     },
     signs = {
       text = {
