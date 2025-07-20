1b73f8c41eb917ae042b089e087bcc4a3418684d
vollowx <vollowx@outlook.com>
2025-07-20 20:01:49 +0800

    feat(qutebrowser): add

---
+++ stow-gui/.config/qutebrowser/config.py
@@ -0,0 +1,10 @@
+import catppuccin
+
+catppuccin.setup(c, "oxocarbon-dark", False)
+config.load_autoconfig(False)
+
+c.auto_save.session = True
+c.colors.webpage.darkmode.enabled = True
+c.colors.webpage.preferred_color_scheme = "dark"
+c.scrolling.smooth = True
+c.tabs.show = "switching"
