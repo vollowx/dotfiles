782dab65d8785dd3ae545a3d623ce4072b1fb890
vollowx <vollowx@outlook.com>
2025-07-22 17:47:52 +0800

    feat(qutebrowser): tweak statusbar padding to be equally high as in zathura

--- stow-gui/.config/qutebrowser/config.py
+++ stow-gui/.config/qutebrowser/config.py
@@ -6,4 +6,5 @@ config.load_autoconfig(False)
 c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.scrolling.smooth = True
+c.statusbar.padding = {"top": 4, "bottom": 4, "left": 4, "right": 4}
 c.tabs.show = "switching"
