871276dc366c03f5d461723a1ccdc526964df892
vollowx <vollowx@outlook.com>
2025-07-22 17:51:51 +0800

    feat(qutebrowser): move tab bar to the bottom

--- stow-gui/.config/qutebrowser/config.py
+++ stow-gui/.config/qutebrowser/config.py
@@ -7,4 +7,5 @@ c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.scrolling.smooth = True
 c.statusbar.padding = {"top": 4, "bottom": 4, "left": 4, "right": 4}
+c.tabs.position = "bottom"
 c.tabs.show = "switching"
