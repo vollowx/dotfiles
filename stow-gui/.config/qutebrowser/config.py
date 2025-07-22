df1fbf52837c0b276e610f5df044c6345367c417
vollowx <vollowx@outlook.com>
2025-07-22 23:40:24 +0800

    feat(qutebrowser): tweak UI

--- stow-gui/.config/qutebrowser/config.py
+++ stow-gui/.config/qutebrowser/config.py
@@ -1,11 +1,11 @@
-import catppuccin
+import theme
 
-catppuccin.setup(c, "oxocarbon-dark", False)
+theme.setup(c, "oxocarbon-dark")
 config.load_autoconfig(False)
 
 c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.scrolling.smooth = True
 c.statusbar.padding = {"top": 4, "bottom": 4, "left": 4, "right": 4}
-c.tabs.position = "bottom"
-c.tabs.show = "switching"
+c.tabs.position = "left"
+c.tabs.width = 300
