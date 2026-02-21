d67607704338d40d71469b8b5efbd09088742588
vollowx <vollowx@outlook.com>
2026-02-21 11:52:53 +0800

    chore(neovim): update

--- home-cli/.config/nvim/lua/configs/quicker.lua
+++ home-cli/.config/nvim/lua/configs/quicker.lua
@@ -4,6 +4,7 @@ local quicker = require('quicker')
 quicker.setup({
   use_default_opts = false,
   opts = { signcolumn = 'auto' },
+  edit = { enabled = true },
   highlight = {
     lsp = false,
     treesitter = true,
@@ -15,17 +16,6 @@ quicker.setup({
     I = icons.diagnostics.DiagnosticSignInfo,
     N = icons.diagnostics.DiagnosticSignHint,
     H = icons.diagnostics.DiagnosticSignHint,
-  },
-  borders = {
-    vert = '│',
-    -- Strong headers separate results from different files
-    strong_header = '─',
-    strong_cross = '┼',
-    strong_end = '┤',
-    -- Soft headers separate results within the same file
-    soft_header = '─',
-    soft_cross = '┼',
-    soft_end = '┤',
   },
   max_filename_width = function()
     return math.max(32, math.ceil(vim.go.columns / 4))
