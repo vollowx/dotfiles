915432bbd9c04c8c5ab9c3ddcd774e66062bbeb3
Vollow <vollowx@outlook.com>
2026-04-12 20:19:10 +0800

    feat(neovim): org instead of vimwiki

--- home-cli/.config/nvim/lua/my/specs/core.lua
+++ home-cli/.config/nvim/lua/my/specs/core.lua
@@ -1,6 +1,19 @@
 return {
-  { src = 'https://github.com/miikanissi/modus-themes.nvim' },
-  { src = 'https://github.com/sainnhe/everforest', data = { on = 'CursorHold' } },
+  {
+    src = 'https://github.com/miikanissi/modus-themes.nvim',
+    data = {
+      postload = function()
+        require('modus-themes').setup({
+          line_nr_column_background = false,
+          sign_column_background = false,
+        })
+      end,
+    },
+  },
+  {
+    src = 'https://github.com/sainnhe/everforest',
+    data = { on = 'CursorHold' },
+  },
   {
     src = 'https://github.com/catppuccin/nvim',
     name = 'catppuccin',
@@ -8,38 +21,16 @@ return {
   },
 
   -- NOTE: `*:c` catches commands and search
-  { src = 'https://github.com/romainl/vim-cool', data = { on = 'ModeChanged', pattern = '*:c' } },
+  {
+    src = 'https://github.com/romainl/vim-cool',
+    data = { on = 'ModeChanged', pattern = '*:c' },
+  },
   { src = 'https://github.com/rhysd/clever-f.vim' },
-  { src = 'https://github.com/tpope/vim-sleuth', data = { on = { 'BufReadPre', 'BufNewFile' } } },
+  {
+    src = 'https://github.com/tpope/vim-sleuth',
+    data = { on = { 'BufReadPre', 'BufNewFile' } },
+  },
   { src = 'https://github.com/tpope/vim-fugitive' },
   { src = 'https://github.com/wakatime/vim-wakatime' },
   { src = 'https://github.com/junegunn/vim-easy-align' },
-
-  -- {
-  --   src = 'https://github.com/vimwiki/vimwiki',
-  --   data = {
-  --     init = function ()
-  --       local wiki_path = '~/Documents/Development/vollowx/idx'
-  --       vim.g.vimwiki_key_mappings = { headers = 0 }
-  --       vim.g.vimwiki_list = {
-  --         {
-  --           path = wiki_path,
-  --           path_html = wiki_path .. '/www',
-  --           syntax = 'default',
-  --           ext = '.wiki',
-  --           custom_wiki2html = '',
-  --         }
-  --       }
-  --     end,
-  --     postload = function()
-  --       vim.api.nvim_create_autocmd('FileType', {
-  --         pattern = 'vimwiki',
-  --         callback = function()
-  --           vim.keymap.set('n', '.', '<Plug>VimwikiAddHeaderLevel', { buffer = true, desc = 'Add Header Level' })
-  --           vim.keymap.set('n', ',', '<Plug>VimwikiRemoveHeaderLevel', { buffer = true, desc = 'Remove Header Level' })
-  --         end,
-  --       })
-  --     end
-  --   },
-  -- },
 }
---
+++ home-cli/.config/nvim/lua/my/specs/orgmode.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    src = 'https://github.com/nvim-orgmode/orgmode',
+    data = {
+      postload = function(_)
+        require('orgmode').setup({
+          org_agenda_files = '~/Documents/Org/**/*',
+          org_default_notes_file = '~/Documents/Org/refile.org',
+          org_todo_keywords = {
+            'TODO',
+            'NEXT',
+            'STARTED',
+            '|',
+            'DONE',
+            'DELEGATED',
+          },
+        })
+
+        vim.lsp.enable('org')
+      end,
+    },
+  },
+}
