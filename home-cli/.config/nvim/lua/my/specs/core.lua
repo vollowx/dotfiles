4a23d94fa9640d95592ad43eafee8245e014cc1e
Vollow <vollowx@outlook.com>
2026-04-19 17:14:30 +0800

    feat(neovim): implement some of the org highlights

--- home-cli/.config/nvim/lua/my/specs/core.lua
+++ home-cli/.config/nvim/lua/my/specs/core.lua
@@ -6,6 +6,26 @@ return {
         require('modus-themes').setup({
           line_nr_column_background = false,
           sign_column_background = false,
+          on_highlights = function(highlight, color)
+            highlight['@org.headline.level1']       = { fg = color.magenta_cooler }
+            highlight['@org.headline.level2']       = { fg = color.magenta_warmer }
+            highlight['@org.headline.level3']       = { fg = color.blue }
+            highlight['@org.headline.level4']       = { fg = color.cyan }
+            highlight['@org.headline.level5']       = { fg = color.green_warmer }
+            highlight['@org.headline.level6']       = { fg = color.yellow }
+            highlight['@org.headline.level7']       = { fg = color.red }
+            highlight['@org.headline.level8']       = { fg = color.magenta }
+            highlight['@org.keyword.todo']          = { fg = color.green }
+            highlight['@org.keyword.done']          = { fg = color.red }
+            highlight['@org.drawer']                = { fg = color.magenta }
+            highlight['@org.agenda.deadline']       = { fg = color.magenta }
+            highlight['@org.agenda.scheduled']      = { fg = color.yellow }
+            highlight['@org.agenda.scheduled_past'] = { fg = color.yellow_faint }
+            highlight['@org.agenda.time_grid']      = { fg = color.fg_dim }
+            highlight['@org.agenda.day']            = { fg = color.cyan }
+            highlight['@org.agenda.today']          = { fg = color.magenta, underline = true }
+            highlight['@org.agenda.weekend']        = { fg = color.magenta }
+          end,
         })
       end,
     },
