4822c8a66b1d55342c39dd0bc22a64a3ea5cb3f2
Vollow <vollowx@outlook.com>
2026-06-28 13:30:09 +0800

    feat(neovim): add substitution

--- neovim/.config/nvim/lua/my/specs/editing.lua
+++ neovim/.config/nvim/lua/my/specs/editing.lua
@@ -12,7 +12,18 @@ return {
       end,
     },
   },
+  {
+    src = 'https://github.com/gbprod/substitute.nvim',
+    data = {
+      postload = function(_)
+        require('substitute').setup({})
+        vim.keymap.set('n', 's', require('substitute').operator, { noremap = true })
+        vim.keymap.set('n', 'ss', require('substitute').line, { noremap = true })
+        vim.keymap.set('n', 'S', require('substitute').eol, { noremap = true })
+        vim.keymap.set('x', 's', require('substitute').visual, { noremap = true })
+      end,
+    },
+  },
   { src = 'https://github.com/tpope/vim-surround' },
-  { src = 'https://github.com/justinmk/vim-sneak' },
   { src = 'https://github.com/junegunn/vim-easy-align' },
 }
